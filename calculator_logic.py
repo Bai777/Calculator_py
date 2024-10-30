2fcb79cee88c4dca581692254eb9086b55bd3bf3
bai_777 <bai_777@mail.ru>
2024-10-30 22:02:09 +0300

    Добавил showwarning Деление на ноль

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,3 +1,5 @@
+from tkinter import messagebox as mb
+
 def add(number1, number2):
     return number1 + number2
 
@@ -12,5 +14,6 @@ def multiply(number1, number2):
 
 def divide(number1, number2):
     if number2 == 0:
+        mb.showerror('Ошибка', 'Делить на ноль нельзя!')
         raise ZeroDivisionError('Деление на ноль')
     return number1 / number2
